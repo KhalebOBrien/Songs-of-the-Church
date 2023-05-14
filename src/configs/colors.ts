f85b804958860b2e0af1054fae461fc1a151c13e
404: CTO_not_found <ebukauche52@gmail.com>
2023-05-14 19:02:21 +0100

    font and color config

---
+++ src/configs/colors.ts
@@ -0,0 +1,6 @@
+export default Object.freeze({
+  white100: '#F5F5F5',
+  purple100: 'FAF9FF',
+  purple200: 'E6E5E8',
+  purple900: '3A2E6E'
+});
---
+++ src/configs/fonts.ts
@@ -0,0 +1,11 @@
+export default Object.freeze({
+  MONTSERRAT_100: 'Montserrat-Thin',
+  MONTSERRAT_200: 'Montserrat-ExtraLight',
+  MONTSERRAT_300: 'Montserrat-Light',
+  MONTSERRAT_400: 'Montserrat-Regular',
+  MONTSERRAT_500: 'Montserrat-Medium',
+  MONTSERRAT_600: 'Montserrat-SemiBold',
+  MONTSERRAT_700: 'Montserrat-Bold',
+  MONTSERRAT_800: 'Montserrat-ExtraBold',
+  MONTSERRAT_900: 'Montserrat-Black',
+});
