e48566f85228fe24e72bde54ad93e05c2aacdda6
418: I'm Caleb <ebukauche52@gmail.com>
2023-05-20 16:54:58 +0100

    search icon

--- src/components/AppSearchBox.tsx
+++ src/components/AppSearchBox.tsx
@@ -1,12 +1,27 @@
-import {View, Text, StyleSheet} from 'react-native';
-// import AppThemeStyles from '../utilities/styles';
+import {View, Text, StyleSheet, TextInput, TextInputProps} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import AppThemeStyles from '../utilities/styles';
+import fonts from '../configs/fonts';
+import Size from '../utilities/useResponsiveSize';
 
 const AppSearchBox = (): JSX.Element => {
   return (
     <View>
-      <Text>AppSearchBox</Text>
+      <Icon name='search1' size={24} style={AppThemeStyles.textColor} ></Icon>
+      <TextInput placeholder='Search here...' />
+      
     </View>
   );
 };
 
 export default AppSearchBox;
+
+const styles = StyleSheet.create({
+  heading: {
+    fontFamily: fonts.MONTSERRAT_600,
+    fontSize: Size.calcWidth(24),
+    fontWeight: '600',
+    marginHorizontal: Size.calcWidth(18),
+    flex: 1,
+  },
+});
