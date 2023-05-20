565adcff55301a478c6d9e9398abe70eb97cd2f8
418: I'm Caleb <ebukauche52@gmail.com>
2023-05-20 13:05:34 +0100

    colors and styles

--- src/configs/colors.ts
+++ src/configs/colors.ts
@@ -1,6 +1,8 @@
 export default Object.freeze({
   white100: '#F5F5F5',
-  purple100: 'FAF9FF',
-  purple200: 'E6E5E8',
-  purple900: '3A2E6E'
+  purple100: '#FAF9FF',
+  purple200: '#E6E5E8',
+  purple900: '#3A2E6E',
+
+  blue900: '#0A0C41'
 });
---
+++ src/utilities/styles.ts
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native';
+import colors from '../configs/colors';
+import fonts from '../configs/fonts';
+import Size from './useResponsiveSize';
+
+const isDarkMode = false;
+
+const styles = StyleSheet.create({
+  textColor: {
+    color: colors.purple900,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  title: {
+    fontFamily: fonts.MONTSERRAT_600,
+    fontSize: Size.calcWidth(20),
+    marginHorizontal: Size.calcWidth(18),
+    flex: 1,
+    color: isDarkMode ? colors.white100 : colors.purple900,
+  },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: Size.calcHeight(70),
+    position: 'relative',
+  },
+  appScreen: {
+    flex: 1,
+    backgroundColor: isDarkMode ? colors.blue900 : colors.white100,
+    paddingHorizontal: Size.calcWidth(30),
+  },
+  view: {
+    flex: 1,
+  },
+});
+
+export default styles;
