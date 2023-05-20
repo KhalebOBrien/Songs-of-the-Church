7857e2dbde4be501512a9b5ea170d7bdf75eb148
418: I'm Caleb <ebukauche52@gmail.com>
2023-05-20 16:33:41 +0100

    app heading

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,6 +2,8 @@ import {View, Text, StyleSheet} from 'react-native';
 import AppSearchBox from './AppSearchBox';
 import AppViewName from './AppViewName';
 import AppThemeStyles from '../utilities/styles';
+import fonts from '../configs/fonts';
+import Size from '../utilities/useResponsiveSize';
 
 export interface AppHeaderProps {
   currentView: string;
@@ -11,7 +13,7 @@ export interface AppHeaderProps {
 const AppHeader = (props: AppHeaderProps): JSX.Element => {
   return (
     <View>
-      <Text style={[AppThemeStyles.textColor, styles.title]}>Songs Of the Church</Text>
+      <Text style={[AppThemeStyles.textColor, styles.heading]}>Songs Of the Church</Text>
       <AppSearchBox/>
       <AppViewName {...props}/>
     </View>
@@ -21,6 +23,11 @@ const AppHeader = (props: AppHeaderProps): JSX.Element => {
 export default AppHeader;
 
 const styles = StyleSheet.create({
-  title: {
+  heading: {
+    fontFamily: fonts.MONTSERRAT_600,
+    fontSize: Size.calcWidth(24),
+    fontWeight: '600',
+    marginHorizontal: Size.calcWidth(18),
+    flex: 1,
   },
 });
